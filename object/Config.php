e685a1bdcc6e084ad368b5443307a7a7965816ee
Michael Alaimo <malaimo923@gmail.com>
2014-11-16 22:16:24 -0500

    rename Main to Config and correct invalid namespace

---
+++ object/Config.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace common\object;
+
+class Config extends \ArrayObject {
+	protected $config = array();
+	
+		
+	public function offsetExists($offset) {
+		return property_exists($this, $offset);
+	}
+	
+	public function offsetGet($offset) {
+		if (!$this->offsetExists($offset)) {
+			throw new \UnexpectedValueException('Offset, '.$offset.', does not exist');
+		}
+		return $this->$offset;
+	}
+	
+	public function offsetSet($offset, $value) {
+		if (property_exists($this, $offset)) {
+			if (isset($this->config[$offset][0])) {
+				if (!($this->$offset = filter_var($value, $this->config[$offset][0], (is_array($this->config[$offset][1]) ? $this->config[$offset][1] : array())))) {
+					throw new \UnexpectedValueException('Invalid value, '.$value.', for offset '.$offset);
+				}
+			} else {
+				throw new \RuntimeException('Filter has not been set for offset '.$offset);
+			}
+		} else {
+			throw new \OutOfBoundsException($offset.' does not exist for '.__CLASS__);
+		}
+	}
+	
+	public function offsetUnset($offset) {
+		$this->$offset = null;
+	}
+	
+	public function __get($offset) {
+		return $this->offsetGet($offset);
+	}
+	
+	public function __set($offset, $value) {
+		return $this->offsetSet($offset, $value);
+	}
+}
