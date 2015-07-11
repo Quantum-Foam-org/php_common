a0b709f91587b347982cacd19a29dec903948f56
Michael Alaimo <malaimo923@gmail.com>
2015-07-11 15:30:45 -0400

    Add db Main and db query Main

--- db/Main.php
+++ db/Main.php
@@ -1,26 +1,24 @@
 <?php
 
-namespace common;
+namespace common\db;
 
 /**
  * Class which extends PHP's PDO.
  * 
  * This should be used when conducting database queries
  */
+class Main extends \PDO {
 
-class Db extends \PDO {
     private static $dbh = null; /// the PDO database handler
-    
-	
+
     public function __construct($dsn, $user, $pass) {
         parent::__construct($dsn, $user, $pass);
     }
-    
-	
-	/**
-	 * Will return the connection to the database
-	 * @return PDO 
-	 */
+
+    /**
+     * Will return the connection to the database
+     * @return PDO 
+     */
     public static function obj() {
         if (!self::$dbh) {
             try {
@@ -33,97 +31,107 @@ class Db extends \PDO {
         
         return self::$dbh;
     }
-	
-	
+    
     /**
-	 * Will return a column, via the columnKey property
-	 * @param String $sql - The sql to run
-	 * @param Array $params - the params to run in a prepared statement
-	 * @param Integer $columnKey - the column of the query to return, default is 0
-	 * @return Array 
-	 */
-    public function fetch_column($sql, array $params = array(), $columnKey = 0) {
-		return $this->get_sth($sql, $params)->fetchColumn($columnKey);
+     * Will return a column, via the columnKey property
+     * @param String $sql - The sql to run
+     * @param Array $params - the params to run in a prepared statement
+     * @param Integer $columnKey - the column of the query to return, default is 0
+     * @return Array 
+     */
+    public function fetchColumn($sql, array $params = array(), $columnKey = 0) {
+        return $this->getSth($sql, $params)->fetchColumn($columnKey);
     }
     
-	
-	/**
-	 * Will return the result of a query by calling the PDOStatement::fetchAll method
-	 * @param String $sql - The sql to run
-	 * @param Array $params - the params to run in a prepared statement
-	 * @return Array 
-	 */
-    public function fetch_all($sql, array $params = array()) {
-        return $this->get_sth($sql, $params)->fetchAll();
+    /**
+     * Will return the result of a query by calling the PDOStatement::fetchAll method
+     * @param String $sql - The sql to run
+     * @param Array $params - the params to run in a prepared statement
+     * @return Array 
+     */
+    public function fetchAll($sql, array $params = array()) {
+        return $this->getSth($sql, $params)->fetchAll();
     }
     
-	
-	/**
-	 * @param String $sql - The sql to run
-	 * @param Array $params - the params to run in a prepared statement
-	 * @param Integer $cursor - cursor position, the default is 0
-	 * @return Array
-	 */
-	public function get_one($sql, array $params = array(), $cursor = 0) {
-        return $this->get_sth($sql, $params)->fetch(\PDO::FETCH_ASSOC, \PDO::FETCH_ORI_NEXT, $cursor);
-	}
-	
-	
-	/**
-	 * @param String $sql - The sql to run
-	 * @param Array $params - the params to run in a prepared statement
-	 * @param Integer $cursor - cursor position, the default is 0
-	 * @return Scalar
-	 */
-    public function get_val($sql, array $params = array(), $cursor = 0) {
-        if (($row = $this->get_one($sql, $params))) {
-            $val =  array_pop($row);
+    /**
+     * @param String $sql - The sql to run
+     * @param Array $params - the params to run in a prepared statement
+     * @param Integer $cursor - cursor position, the default is 0
+     * @return Array
+     */
+    public function getOne($sql, array $params = array(), $cursor = 0) {
+        return $this->getSth($sql, $params)->fetch($this->getAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE), \PDO::FETCH_ORI_NEXT, $cursor);
+    }
+
+    /**
+     * @param String $sql - The sql to run
+     * @param Array $params - the params to run in a prepared statement
+     * @param Integer $cursor - cursor position, the default is 0
+     * @return Scalar
+     */
+    public function getVal($sql, array $params = array(), $cursor = 0) {
+        if (($row = $this->getOne($sql, $params, $cursor))) {
+            $val = array_pop($row);
         }
-		unset($row);
-		
+        unset($row);
+
         return $val;
     }
-    
-	
-	/**
-	 * @param String $table - the name of the table to run the insert
-	 * @param Array $values - A key=>value pair of table field names and values
-	 * @return mixed - lasst insert_id on success
-	 */
+
+    /**
+     * @param String $table - the name of the table to run the insert
+     * @param Array $values - A key=>value pair of table field names and values
+     * @return mixed - lasst insert_id on success
+     */
     public function insert($table, array $values) {
-        $fields = '';
-        $quest = '';
-        foreach ($values as $key => $value) {
-            $fields .= $key.',';
-            $quest .= '?,';
-        }
-        $fields = substr($fields, 0, -1);
-        $quest = substr($quest, 0, -1);
-        
-        $stmt = $this->prepare('INSERT INTO '.$table.' ('.$fields.') VALUES('.$quest.')');
+        $stmt = $this->prepare('INSERT INTO ' . $table . ' (' . implode(', ', array_keys($values)) . ') VALUES(?'.str_repeat(', ?', count($values) - 1).')');
         if ($stmt->exec($values)) {
             $result = $this->lastInsertId();
+        } else {
+            $result = FALSE;
+        }
+
+        return $result;
+    }
+    
+    public function update($table, array $values)
+    {
+        $stmt = $this->prepare('UPDATE ' . $table . ' SET ' . implode(' = ? ,', array_keys($values)).' = ?');
+        if ($stmt->exec($values)) {
+            $result = $this->rowCount();
+        } else {
+            $result = FALSE;
+        }
+
+        return $result;
+    }
+    
+    public function delete($table, array $values)
+    {
+        $stmt = $this->prepare('DELETE FROM ' . $table . ' WHERE ' . implode(' = ? AND ', array_keys($values)).'  = ?');
+        if ($stmt->exec($values)) {
+            $result = $this->rowCount();
         } else {
             $result = FALSE;
         }
         
         return $result;
     }
-    
-	
-	/**
-	 * @param String $sql - The sql to run
-	 * @param Array $params - the params to run in a prepared statement
-	 * @return PDOStatement - the statement of a PDO query
-	 */
-    private function get_sth($sql, array $params = array()) {
+
+    /**
+     * @param String $sql - The sql to run
+     * @param Array $params - the params to run in a prepared statement
+     * @return PDOStatement - the statement of a PDO query
+     */
+    private function getSth($sql, array $params = array()) {
         if (!empty($params)) {
             $sth = $this->prepare($sql);
             $sth->execute($params);
         } else {
             $sth = $this->query($sql);
         }
-        
+
         return $sth;
     }
+
 }
--- db/query/Main.php
+++ db/query/Main.php
@@ -7,8 +7,8 @@ namespace \common\query;
  */
 class Main {
 	
-	private $field_def = array();
-	private $select_fields = array();
+	private $fieldDef = array();
+	private $selectFields = array();
 	private $tables = array();
 	private $joins = array();
 	private $order = '';
@@ -19,16 +19,16 @@ class Main {
 	/**
 	 * @param array $fields - The field definition
 	 */
-	public function add_fields(array $fields) {
+	public function addFields(array $fields) {
 		foreach ($fields as $def) {
-			$this->field_def[] = $def;
-			$this->select_fields[] = (strlen($def[1]) ? $def[1].'.' : '').$def[0];
+			$this->fieldDef[] = $def;
+			$this->selectFields[] = (strlen($def[1]) ? $def[1].'.' : '').$def[0];
 		}
 		unset($fields, $def);
 		return $this;
 	}
 	
-	public function add_tables(array $tables) {
+	public function addTables(array $tables) {
 		foreach ($tables as $def) {
 			if (!isset($def[1], $def[0])) {
 				throw new Exception('Must set table and table alias', -1);
@@ -40,7 +40,7 @@ class Main {
 		return $this;
 	}
 	
-	public function add_joins(array $joins) {
+	public function addJoins(array $joins) {
 		foreach ($joins as $def) {
 			if (isset($def[0], $def[1], $def[2])) {
 				throw new Exception('Must set table alias, join type and join condition', -1);
@@ -52,25 +52,19 @@ class Main {
 		return $this;
 	}
 	
-	public function add_order(array $order) {
-		$this->order_by = 'ORDER BY '.$order[0].' '.(!isset($order[1]) ? 'ASC' : $order[1]);
+	public function addOrder(array $order) {
+		$this->order = 'ORDER BY '.$order[0].' '.(!isset($order[1]) ? 'ASC' : $order[1]);
 		unset($order);
 		return $this;
 	}
 	
-	public function add_limit(array $limit) {
+	public function addLimit(array $limit) {
 		$this->limit = 'LIMIT '.$limit[0].', '.$limit[1];
 		unset($limit);
 		return $this;
 	}
 	
-	public function get_rows() {
-		$query = $this->build_query();
-		
-		return db::obj()->fetch_all($query);
-	}
-	
-	private function build_query() {
+	private function buildQuery() {
 		$select = implode(', '. $this->select_fields);
 		$tables = '';
 		foreach ($this->tables as $alias => $def) {
@@ -85,7 +79,7 @@ class Main {
 		}
 		unset($table, $alias, $def);
 		
-		$order = $this->order_by;
+		$order = $this->order;
 		
 		$limit = $this->limit;
 		
@@ -94,4 +88,7 @@ class Main {
 		return $this;
 	}
 	
+        public function __toString() {
+            return $this->query;
+        }
 }
