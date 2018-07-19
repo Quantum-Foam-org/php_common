e4487483bfa29426061e20779ad69475421169e8
Michael Alaimo <malaimo923@gmail.com>
2018-07-18 23:01:02 -0400

    Updated to work correctly

--- url/Main.php
+++ url/Main.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace \common\url;
+namespace common\url;
 
 class Main {
 	private $url = null;
@@ -12,30 +12,43 @@ class Main {
 		    $this->url = parse_url($this->origUrl);
 			$this->sortQuery();
 			$this->setPath();
+			$this->url['urlString'] = sprintf('%s://%s%s%s%s', $this->url['scheme'], 
+			    $this->url['host'], 
+			    (isset($this->url['path']) && is_array($this->url['path']) ? '/'.implode('/', $this->url['path']) : ''), 
+			    (isset($this->url['query']) && is_array($this->url['query']) ? '?'.http_build_query($this->url['query'], '', '&amp;', PHP_QUERY_RFC3986) : ''), 
+			    (isset($this->url['fragment']) ? '#'.$this->url['fragment'] : ''));
 		} else {
 		    throw new \UnexpectedValueException('Invaild URL:' . $url);
 		}
 	}
 	
 	private function sortQuery() {
-		if (isset($this->query)) {
+	    if (isset($this->url['query'])) {
 			parse_str($this->url['query'], $query);
-			sort($query, SORT_LOCALE_STRING | SORT_STRING);
-			$this->query = $query;
+			foreach ($query as $i => $value) {
+			    if (strpos($i, 'amp;') === 0) {
+			        unset($query[$i]);
+			        $query[str_replace('amp;', '', $i)] = $value;
+			    }
+			}
+			ksort($query, SORT_LOCALE_STRING | SORT_STRING);
+			$this->url['query'] = $query;
 			unset($query);
 		}
 	}
 	
 	public function setPath() {
-		$path = explode('/', $this->url['path']);
-		if ($path !== false) {
-			$this->ur['pathInfo'] = pathinfo($this->url['path']);
-			$path = array_filter($path, function($i) { return strlen(trim($i)); });
-			if (count($path) > 0) {
-				$this->url['path'] = $path;
-			}
-		}
-		unset($path);
+	    if (isset($this->url['path'])) {
+    		$path = explode('/', $this->url['path']);
+    		if ($path !== false) {
+    			$this->url['pathInfo'] = pathinfo($this->url['path']);
+    			$path = array_filter($path, function($i) { return strlen(trim($i)); });
+    			if (count($path) > 0) {
+    				$this->url['path'] = $path;
+    			}
+    		}
+    		unset($path);
+	    }
 	}
 	
 	public function compare(\common\url\Main $url) {
@@ -43,17 +56,17 @@ class Main {
 	}
 	
 	public function __get($name) {
-		if (property_exists($this, $name)) {
-			$result = $this->{$name};
+	    if (array_key_exists($name, $this->url)) {
+			$result = $this->url[$name];
 		} else {
-			\common\logging\writeDebug('Unable to get property: '. $name, -1);
+		    \common\logging\Logger::obj()->writeDebug('Unable to get url index: '. $name, -1);
 		}
 		
 		return $result;
 	}
 	
 	public function __toString() {
-	    return $this->origUrl;
+	    return $this->url['urlString'];
 	}
 	
 	public function __destruct() {
