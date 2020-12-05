d90b57bb2e58fb56be46c6fdb7e9ddff806e7cca
Michael Alaimo <malaimo923@gmail.com>
2020-12-05 10:43:55 -0500

    Add comments

--- errors/ExceptionHandler.php
+++ errors/ExceptionHandler.php
@@ -7,12 +7,20 @@ use common\logging\Logger;
 
 class ExceptionHandler {
 
+    /**
+     * 
+     * @param \common\errors\Exception $e
+     */
     public static function handler(Exception $e) {
         echo "Uncaught exception: ", $e->getMessage(), "\n";
         
         Logger::obj()->writeException($e);
     }
 
+    /**
+     * 
+     * @throws Exception
+     */
     public static function setHander() {
         \set_exception_handler('ExceptionHandler::handler');
 
