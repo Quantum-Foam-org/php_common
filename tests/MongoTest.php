bd21c4034332a5245a26d7b5350c056c0abaff03
Michael Alaimo <michaelalaimo@gmail.com>
2021-02-25 16:23:37 -0500

    use common code, which is autoloaded from the main test library

--- tests/MongoTest.php
+++ tests/MongoTest.php
@@ -2,10 +2,12 @@
 
 namespace common\tests;
 
+use common\logging\Logger as Logger;
+
 class MongoTest {
     
     public function testMongoQuery() : bool {
-        
+        Logger::obj()->write('1message', 1, true);
         return true;
     }
     
